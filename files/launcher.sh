982c0346e88044f1cd3932748ea80314d16b05e9
Ken VanDine <ken.vandine@canonical.com>
2025-09-14 15:42:47 -0400

    Updated packaging

--- files/launcher.sh
+++ files/launcher.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Set the custom banner path if it's not already set
-if [ -z "$CUSTOM_BANNER_PATH" ]; then
-    export CUSTOM_BANNER_PATH="$SNAP/assets/default_banner.png"
+_banner=$(snapctl get banner)
+if [ -f "$_banner" ]; then
+    export CUSTOM_BANNER_PATH="$_banner"
 fi
 
 # Run the main application
-$SNAP/usr/bin/python3 $SNAP/main.py
+$SNAP/gnome-platform/usr/bin/python3.12 $SNAP/main.py
