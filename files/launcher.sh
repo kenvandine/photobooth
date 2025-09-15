ae3db5007933c40fd52aa4fcf58e397b746f7773
Ken VanDine <ken.vandine@canonical.com>
2025-09-14 22:01:17 -0400

    Added device override

--- files/launcher.sh
+++ files/launcher.sh
@@ -11,5 +11,13 @@ if [[ "$_voice" == "true" ]]; then
     export VOICE_ENABLED=1
 fi
 
+export DEVICE_ARGS=""
+
+_device=$(snapctl get device)
+echo $_device
+if [[ -n "$_device" ]]; then
+    export DEVICE_ARGS="--device $_device"
+fi
+
 # Run the main application
-$SNAP/gnome-platform/usr/bin/python3.12 $SNAP/main.py
+$SNAP/gnome-platform/usr/bin/python3.12 $SNAP/main.py $DEVICE_ARGS $@
